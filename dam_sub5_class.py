dab58cf2087f1f248a66e910dcc8825f23f56850
ediakatos <ediakatos@mapaction.org>
2024-07-23 15:07:05 +0000

    Dam data to shp

--- dam_sub5_class.py
+++ dam_sub5_class.py
@@ -4,7 +4,6 @@ import geopandas as gpd
 
 class OSMDamDataDownloader:
     # Fixed class attributes
-    #output_filename = "/home/evangelos/data/data_sub5/swe_sub5/swe_dam_sub5.gpkg"
     osm_key = 'waterway'
     osm_value = 'dam'
 
@@ -13,7 +12,7 @@ class OSMDamDataDownloader:
         self.crs_project = crs_project
         self.crs_global = crs_global
         ox.config(log_console=True, use_cache=True)
-        self.output_filename = f"/home/evangelos/osm-data/{country_code}/{country_code}_phys_dam_pt_s2_osm_pp_dam.gpkg"
+        self.output_filename = f"/home/evangelos/osm-data/{country_code}/dam/{country_code}_phys_dam_pt_s2_osm_pp_dam.shp"
 
     def download_and_process_data(self):
         region_gdf = gpd.read_file(self.geojson_path)
@@ -22,7 +21,7 @@ class OSMDamDataDownloader:
         if geometry.geom_type not in ['Polygon', 'MultiPolygon']:
             raise ValueError("Geometry type not supported. Please provide a Polygon or MultiPolygon.")
 
-        gdf = ox.geometries_from_polygon(geometry, tags={self.osm_key: self.osm_value})
+        gdf = ox.features_from_polygon(geometry, tags={self.osm_key: self.osm_value})
         gdf_projected = gdf.to_crs(epsg=self.crs_project)
         gdf_projected['geometry'] = gdf_projected['geometry'].centroid
         gdf = gdf_projected.to_crs(epsg=self.crs_global)
@@ -36,17 +35,21 @@ class OSMDamDataDownloader:
 
         os.makedirs(os.path.dirname(self.output_filename), exist_ok=True)
 
-        unique_columns = {}
-        for col in gdf.columns:
-            col_truncated = col[:10]
-            if col_truncated in unique_columns:
-                unique_columns[col_truncated] += 1
-                col_truncated = f"{col_truncated}_{unique_columns[col_truncated]}"
-            else:
-                unique_columns[col_truncated] = 1
-            gdf.rename(columns={col: col_truncated}, inplace=True)
+        self.ensure_unique_column_names(gdf)
 
         if not gdf.empty:
-            gdf.to_file(self.output_filename, driver='GPKG')
+            gdf.to_file(self.output_filename, driver='ESRI Shapefile')
         else:
             print("No data to save.")
+
+    def ensure_unique_column_names(self, gdf):
+        # Ensure that column names are unique after truncation
+        new_columns = {}
+        for col in gdf.columns:
+            new_col = col[:10]
+            counter = 1
+            while new_col in new_columns.values():
+                new_col = f"{col[:9]}{counter}"
+                counter += 1
+            new_columns[col] = new_col
+        gdf.rename(columns=new_columns, inplace=True)
